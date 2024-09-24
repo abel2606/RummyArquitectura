ee1514f8952111381bf7f315cf33650804c014b0
abel2606 <150111881+abel2606@users.noreply.github.com>
2024-09-23 23:32:48 -0700

    Add files via upload

---
+++ Rummy/src/main/java/componentes/PanelImagen.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package componentes;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Abe
+ */
+public class PanelImagen extends JPanel {
+
+   private Image backgroundImage;
+
+    public PanelImagen(String ruta) {
+        backgroundImage = new ImageIcon(getClass().getResource(ruta)).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        // Dibujar la imagen de fondo
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+    }
+}
