2c14382068691d99084693cfda08857876388c6e
Victoria Vega <159996124+licoreeee@users.noreply.github.com>
2024-12-05 00:54:28 -0700

    JColorChooser implementado

--- PresentacionRummy/src/main/java/seleccionColorMVC/PantallaSeleccionarColor.java
+++ PresentacionRummy/src/main/java/seleccionColorMVC/PantallaSeleccionarColor.java
@@ -1,13 +1,18 @@
 package seleccionColorMVC;
 
-import java.awt.Color;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import javax.swing.JCheckBox;
 import javax.swing.JOptionPane;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
 
 /**
  *
@@ -22,7 +27,8 @@ public class PantallaSeleccionarColor extends javax.swing.JFrame implements IPan
      */
     public PantallaSeleccionarColor() {
         initComponents();
-        validacionColores();
+        crearBotonesColores();
+//        validacionColores();
         
         setTitle("Rummy - Seleccionar Colores");
         jPanel1.setBackground(new Color(53, 19, 79, 200));
@@ -41,14 +47,6 @@ public class PantallaSeleccionarColor extends javax.swing.JFrame implements IPan
         jLabel4 = new javax.swing.JLabel();
         botonVolver = new componentes.Boton();
         botonConfirmar = new componentes.Boton();
-        checkboxColor1 = new javax.swing.JCheckBox();
-        checkboxColor2 = new javax.swing.JCheckBox();
-        checkboxColor3 = new javax.swing.JCheckBox();
-        checkboxColor4 = new javax.swing.JCheckBox();
-        checkboxColor5 = new javax.swing.JCheckBox();
-        checkboxColor6 = new javax.swing.JCheckBox();
-        checkboxColor7 = new javax.swing.JCheckBox();
-        checkboxColor8 = new javax.swing.JCheckBox();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -82,42 +80,6 @@ public class PantallaSeleccionarColor extends javax.swing.JFrame implements IPan
             }
         });
 
-        checkboxColor1.setBackground(new java.awt.Color(252, 3, 3));
-        checkboxColor1.setForeground(new java.awt.Color(252, 3, 3));
-        checkboxColor1.setText("fc0303");
-        checkboxColor1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                checkboxColor1ActionPerformed(evt);
-            }
-        });
-
-        checkboxColor2.setBackground(new java.awt.Color(7, 3, 252));
-        checkboxColor2.setForeground(new java.awt.Color(7, 3, 252));
-        checkboxColor2.setText("0703fc");
-
-        checkboxColor3.setBackground(new java.awt.Color(252, 244, 3));
-        checkboxColor3.setForeground(new java.awt.Color(252, 244, 3));
-        checkboxColor3.setText("fcf403");
-
-        checkboxColor4.setBackground(new java.awt.Color(24, 252, 3));
-        checkboxColor4.setForeground(new java.awt.Color(24, 252, 3));
-        checkboxColor4.setText("18fc03");
-
-        checkboxColor5.setBackground(new java.awt.Color(252, 3, 140));
-        checkboxColor5.setForeground(new java.awt.Color(252, 3, 140));
-        checkboxColor5.setText("fc038c");
-
-        checkboxColor6.setBackground(new java.awt.Color(169, 3, 252));
-        checkboxColor6.setForeground(new java.awt.Color(169, 3, 252));
-        checkboxColor6.setText("a903fc");
-
-        checkboxColor7.setBackground(new java.awt.Color(252, 107, 3));
-        checkboxColor7.setForeground(new java.awt.Color(252, 107, 3));
-        checkboxColor7.setText("fc6b03");
-
-        checkboxColor8.setBackground(new java.awt.Color(0, 0, 0));
-        checkboxColor8.setText("000000");
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -130,47 +92,15 @@ public class PantallaSeleccionarColor extends javax.swing.JFrame implements IPan
                 .addGap(14, 14, 14))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(28, 28, 28)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 694, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(32, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(41, 41, 41)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(checkboxColor1)
-                            .addComponent(checkboxColor5))
-                        .addGap(123, 123, 123)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(checkboxColor2)
-                            .addComponent(checkboxColor6))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(checkboxColor3)
-                            .addComponent(checkboxColor7))
-                        .addGap(120, 120, 120)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(checkboxColor4)
-                            .addComponent(checkboxColor8))
-                        .addGap(77, 77, 77))))
+                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 694, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(32, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(19, 19, 19)
                 .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(checkboxColor1)
-                    .addComponent(checkboxColor2)
-                    .addComponent(checkboxColor3)
-                    .addComponent(checkboxColor4))
-                .addGap(59, 59, 59)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(checkboxColor5)
-                    .addComponent(checkboxColor6)
-                    .addComponent(checkboxColor7)
-                    .addComponent(checkboxColor8))
-                .addGap(52, 52, 52)
+                .addGap(169, 169, 169)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(botonConfirmar, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(botonVolver, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -190,22 +120,13 @@ public class PantallaSeleccionarColor extends javax.swing.JFrame implements IPan
     }//GEN-LAST:event_botonVolverActionPerformed
 
     private void botonConfirmarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonConfirmarActionPerformed
-        if(checkboxesSeleccionadas.size()==4){
-            for(JCheckBox checkbox : checkboxesSeleccionadas){
-                listaColores.add(Integer.parseInt("0x"+checkbox.getText()));
-            }
-            control.asignarColoresJugador(listaColores);
-            setVisible(false);
-        }
-        else{
-            JOptionPane.showMessageDialog(this, "Debe seleccionar al menos cuatro colores.",
-                    "Alerta", JOptionPane.WARNING_MESSAGE);
-        }
+        listaColores.add(Integer.parseInt("0x"+convertirColorAHex(colorButton1.getSelectedColor())));
+        listaColores.add(Integer.parseInt("0x"+convertirColorAHex(colorButton2.getSelectedColor())));
+        listaColores.add(Integer.parseInt("0x"+convertirColorAHex(colorButton3.getSelectedColor())));
+        listaColores.add(Integer.parseInt("0x"+convertirColorAHex(colorButton4.getSelectedColor())));
+        control.asignarColoresJugador(listaColores);
+        setVisible(false);
     }//GEN-LAST:event_botonConfirmarActionPerformed
-
-    private void checkboxColor1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkboxColor1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_checkboxColor1ActionPerformed
 
     public void crearParametrosMVC(){
         control = ControlColores.getInstance();
@@ -230,47 +151,124 @@ public class PantallaSeleccionarColor extends javax.swing.JFrame implements IPan
         return pantalla;
     }
     
-    private void validacionColores() {
-        JCheckBox[] checkboxesColores = {checkboxColor1, checkboxColor2, checkboxColor3, checkboxColor4,
-                checkboxColor5, checkboxColor6, checkboxColor7, checkboxColor8};
+    private void crearBotonesColores() {
+        colorButton1.setBounds(105, 150, 100, 30);
+        colorButton2.setBounds(255, 150, 100, 30);
+        colorButton3.setBounds(405, 150, 100, 30);
+        colorButton4.setBounds(555, 150, 100, 30);
 
-        for (JCheckBox checkbox : checkboxesColores) {
-            checkbox.addItemListener(new ItemListener() {
-                @Override
-                public void itemStateChanged(ItemEvent e) {
-                    if (checkbox.isSelected()) {
-                        if (checkboxesSeleccionadas.size() >= 4) {
-                            JCheckBox seleccionMasAntigua = checkboxesSeleccionadas.pollFirst();
-                            if (seleccionMasAntigua != null) {
-                                seleccionMasAntigua.setSelected(false);
-                            }
-                        }
-                        checkboxesSeleccionadas.add(checkbox);
-                    } else {
-                        checkboxesSeleccionadas.remove(checkbox);
-                    }
-                }
-            });
-        }
+        jPanel1.add(colorButton1);
+        jPanel1.add(colorButton2);
+        jPanel1.add(colorButton3);
+        jPanel1.add(colorButton4);
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 160, 760, 350));
+
+        pack();
     }
+    
+    private String convertirColorAHex(Color color){
+        return String.format("%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
+    }
+    
+//    private void validacionColores() {
+//        JCheckBox[] checkboxesColores = {checkboxColor1, checkboxColor2, checkboxColor3, checkboxColor4,
+//                checkboxColor5, checkboxColor6, checkboxColor7, checkboxColor8};
+//
+//        for (JCheckBox checkbox : checkboxesColores) {
+//            checkbox.addItemListener(new ItemListener() {
+//                @Override
+//                public void itemStateChanged(ItemEvent e) {
+//                    if (checkbox.isSelected()) {
+//                        if (checkboxesSeleccionadas.size() >= 4) {
+//                            JCheckBox seleccionMasAntigua = checkboxesSeleccionadas.pollFirst();
+//                            if (seleccionMasAntigua != null) {
+//                                seleccionMasAntigua.setSelected(false);
+//                            }
+//                        }
+//                        checkboxesSeleccionadas.add(checkbox);
+//                    } else {
+//                        checkboxesSeleccionadas.remove(checkbox);
+//                    }
+//                }
+//            });
+//        }
+//    }
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private componentes.Boton botonConfirmar;
     private componentes.Boton botonVolver;
-    private javax.swing.JCheckBox checkboxColor1;
-    private javax.swing.JCheckBox checkboxColor2;
-    private javax.swing.JCheckBox checkboxColor3;
-    private javax.swing.JCheckBox checkboxColor4;
-    private javax.swing.JCheckBox checkboxColor5;
-    private javax.swing.JCheckBox checkboxColor6;
-    private javax.swing.JCheckBox checkboxColor7;
-    private javax.swing.JCheckBox checkboxColor8;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
-    private final LinkedList<JCheckBox> checkboxesSeleccionadas = new LinkedList<>();
     private List<Integer> listaColores = new ArrayList<>();
     private static PantallaSeleccionarColor pantalla;
+    ColorChooserButton colorButton1 = new ColorChooserButton(Color.RED);
+    ColorChooserButton colorButton2 = new ColorChooserButton(Color.BLUE);
+    ColorChooserButton colorButton3 = new ColorChooserButton(Color.GREEN);
+    ColorChooserButton colorButton4 = new ColorChooserButton(Color.YELLOW);
+      
+    public class ColorChooserButton extends JButton {
+
+        private Color current;
+
+        public ColorChooserButton(Color c) {
+            setSelectedColor(c); 
+            addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    Color newColor = JColorChooser.showDialog(null, "Choose a color", current);
+                    setSelectedColor(newColor);
+                }
+            });
+        }
+
+        public Color getSelectedColor() {
+            return current;
+        }
+
+        public void setSelectedColor(Color newColor) {
+            setSelectedColor(newColor, true);
+        }
+
+        public void setSelectedColor(Color newColor, boolean notify) {
+
+            if (newColor == null) return;
+
+            current = newColor;
+            setIcon(createIcon(current, 16, 16));
+            repaint();
+
+            if (notify) {
+                // Notify everybody that may be interested.
+                for (ColorChangedListener l : listeners) {
+                    l.colorChanged(newColor);
+                }
+            }
+        }
+
+        public static interface ColorChangedListener {
+            public void colorChanged(Color newColor);
+        }
+
+        private List<ColorChangedListener> listeners = new ArrayList<ColorChangedListener>();
+
+        public void addColorChangedListener(ColorChangedListener toAdd) {
+            listeners.add(toAdd);
+        }
+
+        public static  ImageIcon createIcon(Color main, int width, int height) {
+            BufferedImage image = new BufferedImage(width, height, java.awt.image.BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics = image.createGraphics();
+            graphics.setColor(main);
+            graphics.fillRect(0, 0, width, height);
+            graphics.setXORMode(Color.DARK_GRAY);
+            graphics.drawRect(0, 0, width-1, height-1);
+            image.flush();
+            ImageIcon icon = new ImageIcon(image);
+            return icon;
+        }
+    }
 }
